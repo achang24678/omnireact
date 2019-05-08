242ca3c2dc9761078d3997507dd5f33a14b90b5f
Allen Chang <achang24678@gmail.com>
2019-05-08 00:31:48 -0700

    Start with create-react-app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import logowhite from './img/logo-white.png';
+import logo from './img/logo.png';
+
+
 import * as serviceWorker from './serviceWorker';
+
+
+
+
+const App = () => {
+    return (
+        <header>
+            <nav>
+                <div class="row">
+                    <img src={logowhite} alt="Omnifood logo" class="logo" />
+                    <img src={logo} alt="Omnifood logo" class="logo-black" />
+                    <ul class="main-nav js--main-nav">
+                        <li><a href="https://react-project-2.herokuapp.com/">Project</a></li>
+                        <li><a href="#features">Food delivery</a></li>
+                        <li><a href="#works">How it works</a></li>
+                        <li><a href="#cities">Our cities</a></li>
+                        <li><a href="#plans">Sign up</a></li>
+                    </ul>
+                    <a class="mobile-nav-icon js--nav-icon"><ion-icon name="menu" class="mobile-nav-i"></ion-icon></a>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
